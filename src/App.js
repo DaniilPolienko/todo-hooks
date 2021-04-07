029b5f49e74a81a41f10570589a9f26149b64372
DaniilPolienko <dan.polienko.dev@gmail.com>
2021-04-07 16:18:27 +0300

    Add simple route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React, {useState, useEffect} from 'react'
 import Todos from './components/Todos.js'
-
+import Auth from './components/Auth.js'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import './components/Styles.css'
 
+
 function App(props) {
-  const [appState, setAppState] = useState();
-
-//   useEffect(() => {
-//     const apiUrl = 'https://todo-api-learning.herokuapp.com/';
-//     axios.get(apiUrl).then((resp) => {
-//       const allPersons = resp.data;
-//       setAppState(allPersons);
-//     });
-//   }, [setAppState]);
-
-
-//   async function makeGetRequest() {
-
-//     let task = { name: 'John Doe', done: true };
-
-//     let res = await axios.post('https://todo-api-learning.herokuapp.com/v1/task/{1}', task);
-
-//     let data = res.data;
-//     console.log(data);
-// }
-//   useEffect(()=> {
-//     makeGetRequest();
-//   },[])
-  
-
-
-
-  console.log(appState)
-
   return (
-    <section className="section">
-      <Todos/>
-    </section>
+      <Router>
+        <section className="section">
+            <Switch>
+                <Route exact path="/">
+                    <Todos/>
+                </Route>
+                <Route path="/auth">
+                    <Auth/>
+                </Route>
+            </Switch>
+        </section>
+      </Router>
   );
 }
 
---
+++ src/components/Auth.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Auth = () => {
+    return (
+        <div>
+            <h1>HEllo</h1>
+            <Link to="/">Submit</Link>
+        </div>
+    );
+}
+
+export default Auth;
