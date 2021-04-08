38c4ef3bbc788286bda2110256566aeb9873c765
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-08 17:24:19 +0300

    Add signup input change

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import Todos from './components/Todos.js'
-import Auth from './components/Auth.js'
-import SignUp from './components/SignUp.js'
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
-import './App.css';
-import './components/Styles.css'
-
+import React from "react";
+import Todos from "./components/Todos.js";
+import Auth from "./components/Auth.js";
+import SignUp from "./components/SignUp.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "./components/Styles.css";
 
 function App(props) {
-
   return (
-      <Router>
-
-        <section className="section">
-            <Switch>
-                <Route exact path="/">
-                    <Todos/>
-                </Route>
-                <Route path="/auth">
-                    <Auth/>
-                </Route>
-                <Route path="/todo-hooks">
-                    <Auth/>
-                </Route>
-                <Route path="/signup">
-                    <SignUp/>
-                </Route>
-            </Switch>
-        </section>
-      </Router>
+    <Router>
+      <section className="section">
+        <Switch>
+          <Route exact path="/">
+            <Todos />
+          </Route>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <Route path="/todo-hooks">
+            <Auth />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </section>
+    </Router>
   );
 }
-
 
 export default App;
