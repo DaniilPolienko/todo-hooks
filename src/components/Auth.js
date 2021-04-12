6d77f8dd12ef20d5106bd153a75b77d6d809dc63
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-12 15:48:53 +0300

    Fix auth interceptors

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -81,7 +81,7 @@ export default function SignIn() {
     },
     (error) => {
       if (error) {
-        setError(error.preview.data.error || error.response.data.errors);
+        setError(error.response.data.error || error.response.data.errors);
         setOpen(true);
       }
       return Promise.reject(error);
