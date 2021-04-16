02b31da9757ca837de502910436a4062929eb8f5
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-16 12:02:05 +0300

    Add hashrouter

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React from "react";
 import Todos from "./components/Todos.js";
 import Auth from "./components/Auth.js";
 import SignUp from "./components/SignUp.js";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
 import "./components/Styles.css";
 
 function App(props) {
   return (
-    <Router>
-      <section className="section">
-        <Switch>
-          <Route exact path="/todos">
-            <Todos />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-        </Switch>
-      </section>
-    </Router>
+    <HashRouter basename="/todos">
+      <Router>
+        <section className="section">
+          <Switch>
+            <Route exact path="/todos">
+              <Todos />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+          </Switch>
+        </section>
+      </Router>
+    </HashRouter>
   );
 }
 export default App;
