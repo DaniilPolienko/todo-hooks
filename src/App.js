f8c94788ab30d7293b54571b9d239e47d5976ec4
daniilpolienko <dan.polienko.dev@gmail.com>
2021-04-16 14:40:49 +0300

    attemp3

--- src/App.js
+++ src/App.js
@@ -8,22 +8,20 @@ import "./components/Styles.css";
 
 function App(props) {
   return (
-    <Router basename="/todo-hooks">
-      <Router>
-        <section className="section">
-          <Switch>
-            <Route exact path="/todo-hooks">
-              <Todos />
-            </Route>
-            <Route path="/auth">
-              <Auth />
-            </Route>
-            <Route path="/signup">
-              <SignUp />
-            </Route>
-          </Switch>
-        </section>
-      </Router>
+    <Router>
+      <section className="section">
+        <Switch>
+          <Route exact path="/todo-hooks">
+            <Todos />
+          </Route>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </section>
     </Router>
   );
 }
